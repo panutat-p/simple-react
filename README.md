70c693876c56650d80f1486265eb1194c56847b2
Panutat <osprior_panutat.preechawuttidet@krungthai.com>
2023-12-12 18:06:51 +0700

    Implement TailwindCSS

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # simple-react
 
+https://react.dev/learn
+
 https://vitejs.dev/guide/#scaffolding-your-first-vite-project
+
+https://placehold.co
 
 ## Packages
 
@@ -8,4 +12,22 @@ https://vitejs.dev/guide/#scaffolding-your-first-vite-project
 npm i -D prettier
 touch .prettierignore
 touch .prettierrc.yaml
+```
+
+## Tailwind CSS
+
+https://tailwindcss.com/docs/guides/vite
+
+```shell
+npm i -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+touch src/index.css
+```
+
+## APIs
+
+https://browsersync.io/#install
+
+```shell
+browser-sync start --server --files "*.html, css/*.css"
 ```
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <main className="container mx-auto">
+        <h1 className="text-5xl font-bold underline mt-10 mb-5">Simple React</h1>
+        <div className="flex flex-row">
+          <img src={viteLogo} className="w-20" alt="Vite logo" />
+          <img src={reactLogo} className="w-20" alt="React logo" />
+        </div>
+        <div className="mt-5 mb-5">
+          <MyButton />
+        </div>
+        <img src="https://placehold.co/300x200/000000/FFF" alt="simple placeholder" />
+      </main>
     </>
   )
 }
 
-export default App
+function MyButton() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button
+        className="px-8 py-4 text-2xl text-white bg-blue-500 rounded"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        count is {count}
+      </button>
+    </>
+  )
+}
